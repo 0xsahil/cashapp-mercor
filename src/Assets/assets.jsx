45884c1eb2cdb9d89b6616bc0bc3879bcb935341
0xsahil <85027015+0xsahil@users.noreply.github.com>
2023-07-28 13:02:29 +0530

    Project Init

---
+++ src/Assets/assets.jsx
@@ -0,0 +1,11 @@
+// Navbar Items
+import cashAppLogo from "./cashAppLogo.svg";
+import eyeButton from "./eyeButton.svg";
+
+export {
+  // Navbar Items
+  cashAppLogo,
+  eyeButton,
+
+  // Home items
+};
